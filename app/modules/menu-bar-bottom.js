b89e61d4c652d423622844efb1bdcada9b41b425
Joseph Bergen <jbergen@gmail.com>
2013-01-16 17:01:55 -0500

    files added

---
+++ app/modules/menu-bar-bottom.js
@@ -0,0 +1,115 @@
+define([
+    "app",
+    // Libs
+    "backbone"
+],
+
+function(app, Backbone) {
+
+    // Create a new module
+    var Citations = {};
+
+    // This will fetch the tutorial template and render it.
+    Citations.View = Backbone.View.extend({
+        
+        visible : true,
+        hover: false,
+        playing: false,
+
+        template: "menu-bar-bottom",
+
+        className: "ZEEGA-player-citations",
+
+        serialize: function() {
+            return this.model.data.toJSON();
+        },
+
+        initialize: function() {
+            /* update the arrow state whenever a frame is rendered */
+            this.model.on("frame_rendered", this.updateCitations, this);
+            this.model.on("data_loaded", this.render, this);
+            this.model.on("play", this.onPlay, this );
+            this.model.on("pause", this.onPause, this );
+        },
+
+        onPlay: function() {
+            this.$("#project-play-pause i").addClass("icon-pause").removeClass("icon-play");
+        },
+
+        onPause: function() {
+            this.$("#project-play-pause i").addClass("icon-play").removeClass("icon-pause");
+            this.fadeIn();
+        },
+
+        updateCitations: function( info ) {
+            var layersToCite = _.map( info.layers, function(layer){
+                if(layer.citation) return layer;
+                return false;
+            });
+
+            this.$(".ZEEGA-citations-primary").empty();
+            _.each( _.compact(layersToCite), function(layer){
+                var citation = new CitationView({ model: new Backbone.Model(layer) });
+                this.$(".ZEEGA-citations-primary").append(citation.el);
+                citation.render();
+            }.bind( this ));
+        },
+
+        events: {
+            "mouseenter": "onMouseenter",
+            "mouseleave": "onMouseleave",
+            "click #project-play-pause": "playpause"
+        },
+
+        fadeOut: function() {
+            if(this.visible && !this.hover && app.player.status != "paused" ) {
+                this.visible = false;
+                this.$el.fadeOut();
+            }
+        },
+     
+        fadeIn: function() {
+            if(!this.visible) {
+                this.visible = true;
+                this.$el.fadeIn();
+            }
+        },
+
+        onMouseenter: function() {
+            this.hover = true;
+        },
+
+        onMouseleave: function() {
+            this.hover = false;
+        },
+
+        playpause: function() {
+            if ( this.model.status == "paused") {
+                this.model.play();
+            } else {
+                this.model.pause();
+            }
+            return false;
+        }
+    });
+
+    var CitationView = Backbone.View.extend({
+        tagName: "li",
+        template: "citation",
+
+        serialize: function() {
+            return this.model.toJSON();
+        },
+
+        events: {
+            "hover": "onHover"
+        },
+
+        onHover: function() {
+            this.$("i").toggleClass("loaded");
+        }
+  
+    });
+
+    return Citations;
+});
---
+++ app/modules/menu-bar-top.js
@@ -0,0 +1,119 @@
+define([
+    "app",
+    // Libs
+    "backbone"
+],
+
+function(app, Backbone) {
+
+    // Create a new module
+    var MenuBar = {};
+
+    // This will fetch the tutorial template and render it.
+    MenuBar.View = Backbone.View.extend({
+        
+        visible: true,
+        hover: false,
+
+        template: "menu-bar-top",
+
+        className: "ZEEGA-player-menu-bar",
+
+        serialize: function() {
+            return this.model.data.toJSON();
+        },
+
+        initialize: function() {
+            this.model.on("data_loaded", this.render, this);
+            this.model.on("sequence_enter", this.onEnterSequence, this );
+        },
+
+        onEnterSequence: function( info ) {
+            this.updateDescription( info );
+        },
+
+        updateDescription: function( info ) {
+            /* update the sequence title in the menu bar */
+            // var def = /Sequence ([0-9]*)/g.test(info.title);
+            // var seqTitle = def ? "" : " - "+ info.title;
+            // this.$(".sequence-description").text(seqTitle);
+
+        },
+
+        events: {
+            "click #project-share": "share",
+            "click #project-credits": "credits",
+            "click #project-fullscreen-toggle": "toggleFullscreen",
+            "mouseenter": "onMouseenter",
+            "mouseleave": "onMouseleave"
+        },
+
+        share: function() {
+            this.model.pause();
+            this.$(".slide-menu").toggle();
+            return false;
+        },
+
+        credits: function() {
+            return false;
+        },
+
+        toggleFullscreen: function() {
+            if ( app.state.get("fullscreen") ) {
+                this.leaveFullscreen();
+            } else {
+                this.goFullscreen();
+            }
+            return false;
+        },
+
+        goFullscreen : function() {
+            app.state.set("fullscreen", true );
+            docElm = document.getElementById("main");
+              
+            if ( docElm.requestFullscreen ) docElm.requestFullscreen();
+            else if ( docElm.mozRequestFullScreen ) docElm.mozRequestFullScreen();
+            else if ( docElm.webkitRequestFullScreen ) docElm.webkitRequestFullScreen();
+
+            this.$("#project-fullscreen-toggle i")
+                .removeClass("icon-resize-full")
+                .addClass("icon-resize-small");
+        },
+
+        leaveFullscreen : function() {
+            app.state.set("fullscreen", false );
+            if ( document.exitFullscreen )        document.exitFullscreen();
+            else if ( document.mozCancelFullScreen )    document.mozCancelFullScreen();
+            else if ( document.webkitCancelFullScreen )   document.webkitCancelFullScreen();
+
+            this.$("#project-fullscreen-toggle i")
+                .addClass("icon-resize-full")
+                .removeClass("icon-resize-small");
+        },
+
+        fadeOut: function() {
+            if( this.visible && !this.hover && app.player.status != "paused") {
+                this.visible = false;
+                this.$el.fadeOut();
+            }
+        },
+     
+        fadeIn: function() {
+            if ( !this.visible ) {
+                this.visible = true;
+                this.$el.fadeIn();
+            }
+        },
+
+        onMouseenter: function() {
+            this.hover = true;
+        },
+
+        onMouseleave: function() {
+            this.hover = false;
+        }
+
+    });
+
+    return MenuBar;
+});
