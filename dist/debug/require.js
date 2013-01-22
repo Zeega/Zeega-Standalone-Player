e7015938387c679734774fa5485ab156d16de7a2
Joseph Bergen <jbergen@gmail.com>
2013-01-22 18:47:33 -0500

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -40496,14 +40496,13 @@ function( Zeega, SequenceCollection ) {
                         return frame.get( value );
                     });
 
-                    linkedFrames = _.flatten( linkedFrames );
+                    linkedFrames = _.compact( _.flatten( linkedFrames ) );
 
                     _.each( _.uniq( linkedFrames ), function( frameID ) {
                         var targetFrame = this.getFrame( frameID );
                         
                         commonLayers[ frameID ] = _.intersection( targetFrame.get("layers"), frame.get("layers") );
                     }, this );
-
                     frame.put("common_layers", commonLayers );
                 }, this );
             }, this );
