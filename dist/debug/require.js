89fdd4b0326b09a2e1e767299a17cf2434df3167
James Burns <jameburn@gmail.com>
2013-06-05 16:10:39 -0700

    updates

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -49308,7 +49308,6 @@ function( Zeega, SequenceCollection ) {
                     }
 
                     if( nextSequence ) {
-                        console.log(this, nextSequence );
                         preloadTargets.push( this.sequences.get( nextSequence ).get("frames")[0] );
                     }
 
@@ -49363,7 +49362,13 @@ function( Zeega, SequenceCollection ) {
                     _.each( _.uniq( linkedFrames ), function( frameID ) {
                         var targetFrame = this.getFrame( frameID );
                         
-                        commonLayers[ frameID ] = _.intersection( targetFrame.get("layers"), frame.get("layers") );
+                        if(targetFrame.get("layers") && frame.get("layers") && targetFrame.get("layers").length && frame.get("layers").length ){
+
+                            commonLayers[ frameID ] = _.intersection( targetFrame.get("layers"), frame.get("layers") );
+                        } else {
+                            commonLayers = [];
+                        }
+
                     }, this );
                     frame.put("common_layers", commonLayers );
                 }, this );
@@ -49425,15 +49430,18 @@ function() {
 
     Parser[ type ].validate = function( response ) {
         var project = response.project;
+
         if ( project.sequences && project.frames && project.layers ) {
             return true;
         }
         return false;
     };
 
-    // no op. projects are already formatted
+
     Parser[type].parse = function( response, opts ) {
+        
         return response.project;
+
     };
 
     return Parser;
