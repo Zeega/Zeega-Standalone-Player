9f6c22d8f79d918e197bf6732808f599a3a0d1c7
jbergen <jbergen@gmail.com>
2013-06-06 15:04:59 -0700

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -37156,66 +37156,11 @@ function() {
             response.project.sequences[0].frames.push( endId );
         }
 
-
         return response.project;
-
-
-        // if ( opts.endPage ) {
-        //     var endId, lastPageId, lastPage, endPage, endLayers;
-
-        //     endId = -1;
-        //     lastPageId = response.sequences[0].frames[ response.sequences[0].frames.length - 1 ];
-        //     lastPage = _.find( response.frames, function( frame ) {
-        //         return frame.id == lastPageId;
-        //     });
-        //     endPage = _.extend({}, lastPage );
-
-        //     // only allow images, color layers
-        //     endLayers = _.filter(response.layers, function( layer ) {
-        //         return _.include(["Image", "Rectangle"], layer.type ) && _.include( endPage.layers, layer.id );
-        //     });
-
-        //     endPage.layers = _.pluck( endLayers, "id");
-        //     endPage.layers.push( endId );
-
-        // removeDupeSoundtrack( response.project );
-
-        // if ( opts.endPage ) {
-        //     var endId, lastPageId, lastPage, endPage, endLayers;
-
-        //     endId = -1;
-        //     lastPageId = response.sequences[0].frames[ response.sequences[0].frames.length - 1 ];
-        //     lastPage = _.find( response.frames, function( frame ) {
-        //         return frame.id == lastPageId;
-        //     });
-        //     endPage = _.extend({}, lastPage );
-
-        //     // only allow images, color layers
-        //     endLayers = _.filter(response.layers, function( layer ) {
-        //         return _.include(["Image", "Rectangle"], layer.type ) && _.include( endPage.layers, layer.id );
-        //     });
-
-        //     endPage.layers = _.pluck( endLayers, "id");
-        //     endPage.layers.push( endId );
-        //     // add layer to layer array
-        //     response.layers.push({
-        //         id: endId,
-        //         type: "EndPageLayer"
-        //     });
-            
-        //     endPage.id = endId;
-        //     response.frames.push( endPage );
-        //     response.sequences[0].frames.push( endId );
-        // }
-
-        // return response;
-        //}
-
     };
 
     return Parser;
 });
-
 define('engine/data-parsers/zeega-project-published',["lodash"],
 
 function() {
