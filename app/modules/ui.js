8cbe9e28d1fc2677a23eb351d4ee270fe53af7cb
James Burns <jameburn@gmail.com>
2013-06-11 10:30:21 -0700

    iframe attr updates

--- app/modules/ui.js
+++ app/modules/ui.js
@@ -50,8 +50,8 @@ function( app, Backbone, Loader, Controls, MenuBarBottom, MenuBarTop, PauseView,
 
             this.insertView("#overlays", this.citations );
             this.insertView("#overlays", this.menuBar );
-            console.log("the window: ",window);
-            if( window == window.top || window.endpage ){
+            console.log( window.frameElement.getAttribute("endpage") );
+            if( window == window.top || window.frameElement.getAttribute("endpage") ){
                 this.endPage = new EndPage.View({ model: app.player });
                 this.insertView("#overlays", this.endPage );
             }
