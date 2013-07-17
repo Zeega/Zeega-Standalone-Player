59b2a978c684acd6de729e1fb490d3828629e333
jbergen <jbergen@gmail.com>
2013-07-17 16:33:03 -0700

    cleaned app

--- app/app.js
+++ app/app.js
@@ -12,9 +12,6 @@ define([
 
 function( $, _, Backbone, State, Spinner ) {
     
-    var meta = $("meta[name=zeega]");
-    // Provide a global location to place configuration settings and module
-    // creation.
     var app = {
         // The root path to run the application.
         root: "/",
@@ -33,25 +30,24 @@ function( $, _, Backbone, State, Spinner ) {
         }
     };
 
-    var opts = {
-        lines: 13, // The number of lines to draw
-        length: 10, // The length of each line
-        width: 4, // The line thickness
-        radius: 30, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#FFF', // #rgb or #rrggbb
-        speed: 1, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: 'auto', // Top position relative to parent in px
-        left: 'auto' // Left position relative to parent in px
-    };
-    app.spinner = new Spinner(opts);
+    app.spinner = new Spinner({
+            lines: 13, // The number of lines to draw
+            length: 10, // The length of each line
+            width: 4, // The line thickness
+            radius: 30, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            color: '#FFF', // #rgb or #rrggbb
+            speed: 1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto' // Left position relative to parent in px
+        });
 
     // Localize or create a new JavaScript Template object.
     var JST = window.JST = window.JST || {};
@@ -73,14 +69,12 @@ function( $, _, Backbone, State, Spinner ) {
             var done;
             // Concatenate the file extension.
             path = path + ".html";
-
             // If cached, use the compiled template.
             if (JST[path]) {
                 return JST[path];
             } else {
                 // Put fetch into `async-mode`.
                 done = this.async();
-
                 // Seek out the template asynchronously.
                 return $.ajax({ url: app.root + path }).then(function(contents) {
                     done(JST[path] = _.template(contents));
