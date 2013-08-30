4acb92da344194f57723415b4dd7432fb7172e38
jbergen <jbergen@gmail.com>
2013-08-30 12:45:16 -0700

    added common sub

--- app/app.js
+++ app/app.js
@@ -5,29 +5,16 @@ define([
     "backbone",
 
     "modules/state",
-    "engineVendor/spin",
+    "common/_app.common",
     // Plugins.
     "plugins/backbone.layoutmanager"
 ],
 
-function( $, _, Backbone, State, Spinner ) {
+function( $, _, Backbone, State, _App ) {
     
     var app = {
         // The root path to run the application.
         root: "/",
-        metadata: $("meta[name=zeega]").data(),
-
-        getWebRoot: function() {
-            return "http:" + this.metadata.hostname + this.metadata.directory;
-        },
-
-        getApi: function() {
-            return this.getWebRoot() + "api/";
-        },
-
-        getUserId: function() {
-            return this.metadata.userId;
-        },
 
         isEmbed: function() {
             var isEmbed;
@@ -59,42 +46,9 @@ function( $, _, Backbone, State, Spinner ) {
         state: new State(),
 
         Backbone: Backbone,
-        $: $,
-        
-        emit: function( event, args ) {
-            // other things can be done here as well
-            this.trigger( event, args );
-        }
+        $: $
+
     };
-
-    app.spinner = new Spinner({
-            lines: 13, // The number of lines to draw
-            length: 10, // The length of each line
-            width: 4, // The line thickness
-            radius: 30, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 0, // The rotation offset
-            direction: 1, // 1: clockwise, -1: counterclockwise
-            color: '#FFF', // #rgb or #rrggbb
-            speed: 1, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: 'auto', // Top position relative to parent in px
-            left: 'auto' // Left position relative to parent in px
-        });
-
-    app.spin = function( el ) {
-        var target = el || app.layout.el;
-
-        app.spinner.spin( target );
-    }
-
-    app.spinStop = function() {
-        app.spinner.stop();
-    }
 
     // Localize or create a new JavaScript Template object.
     var JST = window.JST = window.JST || {};
@@ -106,23 +60,20 @@ function( $, _, Backbone, State, Spinner ) {
         return this.set.apply( this, args );
     };
 
-
     Backbone.Layout.configure({
         // Allow LayoutManager to augment Backbone.View.prototype.
         manage: true,
 
         fetch: function( path ) {
-            // Initialize done for use in async-mode
             var done;
-            // Concatenate the file extension.
+
             path = path + ".html";
-            // If cached, use the compiled template.
+
             if (JST[path]) {
                 return JST[path];
             } else {
-                // Put fetch into `async-mode`.
                 done = this.async();
-                // Seek out the template asynchronously.
+
                 return $.ajax({ url: app.root + path }).then(function(contents) {
                     done(JST[path] = _.template(contents));
                 });
@@ -130,9 +81,5 @@ function( $, _, Backbone, State, Spinner ) {
         }
     });
     
-    // Mix Backbone.Events, modules, and layout management into the app object.
-    return _.extend(app, {
-        
-    }, Backbone.Events);
-
+    return _.extend(app, _App, Backbone.Events);
 });
