a8a4d2f35d61b9e6dfbeb8969086df02eb4072fe
jbergen <jbergen@gmail.com>
2013-09-16 11:19:46 -0700

    with debug logs

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -40234,6 +40234,9 @@ function( app, Player, PlayerUI, Analytics ) {
             loops = !app.isEmbed() ? true : app.getIframeAttributes().loop;
             hasEndpage = !loops && !( projectData && projectData.project.remix.descendants.length ) && app.isEmbed();
 
+
+console.log("INIT PLAYER", loops, hasEndpage, app.getIframeAttributes(), projectData)
+
             app.player = new Player({
                 // debugEvents: true,
 
